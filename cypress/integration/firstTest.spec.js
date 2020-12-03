ce4b5e9ef465864456065faa739953cf1051b573
Soria <soriasayer@gmail.com>
2020-12-03 11:30:48 +0100

    Cypress: Working with checkboxes.

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -127,4 +127,42 @@ describe('The first suite', () => {
     })
   })
 
+  it('radio button', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+     cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioBtn => {
+        cy.wrap(radioBtn)
+          .first()
+          .check({force: true})
+          .should('be.checked')
+
+        cy.wrap(radioBtn)
+          .eq(1)
+          .check({force: true})
+
+        cy.wrap(radioBtn)
+          .first()
+          .should('not.be.checked')
+
+        cy.wrap(radioBtn)
+          .eq(2)
+          .should('be.disabled')
+    })
+  })
+
+  it.only('check boxes', () => {
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Toastr').click()
+
+    // it check the unchecked button
+    cy.get('[type="checkbox"]').check({force: true})
+    // it unchecked the checked and check the uncheck
+    cy.get('[type="checkbox"]').eq(0).click({force: true})
+    cy.get('[type="checkbox"]').eq(1).click({force: true})
+    cy.get('[type="checkbox"]').eq(2).click({force: true})
+  })
+
 })
